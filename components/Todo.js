49a096bae99723c23ec0161efb51b2f5dc72609d
dariu <darius.studiosuipod@gmail.com>
2024-12-03 09:49:40 -0600

    complete video 5 for project 7: show selected dates on list items and add unigue IDs for each one

--- components/Todo.js
+++ components/Todo.js
@@ -27,17 +27,28 @@ class Todo {
       .querySelector(".todo")
       .cloneNode(true);
 
-    const todoNameEl = this._todoElement.querySelector(".todo__name");
-    const todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+    this._todoDate = this._todoElement.querySelector(".todo__date");
     this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
-    todoNameEl.textContent = this._data.name;
+    this._todoNameEl.textContent = this._data.name;
 
-    this._generateCheckBoxEl();
-    this._setEventListeners();
+    this._dueDate = new Date(this._data.date);
+    if (!isNaN(this._dueDate)) {
+      this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
+        "en-US",
+        {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }
+      )}`;
 
-    return this._todoElement;
+      this._generateCheckBoxEl();
+      this._setEventListeners();
+
+      return this._todoElement;
+    }
   }
 }
-
 export default Todo;
