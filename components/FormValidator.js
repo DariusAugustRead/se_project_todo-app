25e274ff770fc58813f222d271f5a323787eb82a
dariu <darius.studiosuipod@gmail.com>
2024-12-04 11:38:07 -0600

    discord help

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,6 +1,7 @@
 class FormValidator {
   constructor(settings, formEl) {
     this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
     this._submitButtonSelector = settings.submitButtonSelector;
     this._errorClass = settings.errorClass;
     this._inputErrorClass = settings.inputErrorClass;
@@ -8,7 +9,60 @@ class FormValidator {
     this._formEl = formEl;
   }
 
-  enableValidation() {}
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(
+        this._formEl,
+        this._inputElement,
+        this._inputElement.validationMessage,
+        this._settings
+      );
+    } else {
+      hideInputError(formEl, inputElement, settings);
+    }
+  }
+
+  _hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState(buttonElement) {
+    if (this._hasInvalidInput(this._inputList)) {
+      buttonElement.classlist.add(this._inactiveButtonClass);
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classlist.remove(this._inactiveButtonClass);
+      buttonElement.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    const buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState(buttonElement);
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState(inputList, buttonElement, settings);
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    this._setEventListeners();
+  }
 }
 
 export default FormValidator;
